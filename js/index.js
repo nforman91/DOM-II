25fbe8ea4b7a25bdccccf7e6a57e8a7ab1b2f972
Nathan <nathan.forman@gmail.com>
2021-08-03 17:26:15 -0700

    Almost done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-//1.
+//1. mouseover
 const navChg = document.querySelector('.nav-link');
 navChg.addEventListener('mouseover', function(event){
     navChg.style.color = "white";
@@ -8,7 +8,7 @@ navChg.addEventListener('mouseover', function(event){
     navChg.style.transform = "rotate(20deg)"
 })
 
-//2.
+//2. keydown
 function rKey(event){
     if(event.key === 'r'){
         alert('Ready!')
@@ -16,19 +16,19 @@ function rKey(event){
 }
 document.addEventListener('keydown', rKey)
 
-//3.
+//3.dblclick
 const footerChg = document.querySelector('.footer');
 footerChg.addEventListener('dblclick', function (event) {
     footerChg.style.backgroundColor = "green";
 })
 
-//4.
-const headerChg = document.querySelector('h2');
-headerChg.addEventListener('click', function(event){
-    headerChg.style.transform = "rotate(20deg)";
+//4. click
+const heading2Chg = document.querySelector('h2');
+heading2Chg.addEventListener('click', function(event){
+    heading2Chg.style.transform = "rotate(20deg)";
 })
 
-//5.
+//5. keyup
 function wKey(event) {
     if (event.key === 'w') {
         alert('Winning!')
@@ -36,69 +36,48 @@ function wKey(event) {
 }
 document.addEventListener('keyup', wKey)
 
-//6.
+//6. mouseenter
 const imgChg = document.querySelector('img')
 imgChg.addEventListener('mouseenter', function (event) {
     imgChg.style.width = "50%";
 })
 
-//7.
-const titleChg = document.querySelector('h1')
-titleChg.addEventListener('hover', function (event) {
-    console.log(titleChg)
-    titleChg.style.textContent = "Changing title works";
+//7. mousemove
+const buttonChg = document.querySelector('.btn')
+buttonChg.addEventListener('mousemove', function (event) {
+    buttonChg.style.transform = "rotate(20deg)";
 })
 
-//8.
-// Chg.addEventListener('', function (event) {
-//     Chg.style. = "";
-// })
+//8. keypress
+function eKey(event) {
+    if (event.key === 'e') {
+        alert('Exciting!')
+    }
+}
+document.addEventListener('keypress', eKey)
 
-//9.
-// Chg.addEventListener('', function (event) {
-//     Chg.style. = "";
-// })
+//9. mouseleave
+const heading1Chg = document.querySelector('h1')
+heading1Chg.addEventListener('mouseleave', function (event) {
+    heading1Chg.style.fontSize = "150%";
+})
 
-//10.
-// Chg.addEventListener('', function (event) {
-//     Chg.style. = "";
-// })
+//10. copy
+const headerChg = document.querySelector('header')
+headerChg.addEventListener('copy', function (event) {
+    headerChg.style.backgroundColor = "red";
+})
 
 //X. Stop Propagation
 const textChg = document.querySelector('.text-content');
 textChg.addEventListener('click', function(event){
-    console.log("Inside text change on click")
     textChg.textContent = "Text change works";
     event.stopPropagation();
 })
 
 //Y. Prevent Default
-// const navChg = document.querySelector('a');
-// navChg.addEventListener('', function(event){
-//      navChg.stoprefresh
-//      event.preventDefault();
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//TESTSTUFF
-// const txt = document.querySelector('inverse-content .text-content')
-// txt.addEventListener('mousemove', event => {
-//     txt.style.backgroundColor = "red";
-//     txt.style.color = "white";
+// const textChg = document.querySelector('.text-content');
+// textChg.addEventListener('click', function (event) {
+//     textChg.textContent = "Text change works";
+//     event.preventDefault();
 // })
